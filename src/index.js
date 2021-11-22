dd53812560b18bbd9ad14c8c6db8ea9fb606752b
Armic <71387232+ItzArmic@users.noreply.github.com>
2021-11-21 19:35:09 -0800

    Added user agent thing bla bla

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const hypixel = new Hypixel.Client(config.ApiKey);
 
 setInterval(() => { 
     hypixel.getPlayer(config.username).then(player => { 
-        axios({method: 'patch', url: "https://discordapp.com/api/v6/users/@me", headers: { authorization: config.token }, data: { bio: "`` 🔴🟢🟡       Stats        ⎯⠀❐ ⤬ `` ``          " + "   " + player.stats.bedwars.level + "⭐                  \n" +                      "             Wins:  "+ player.stats.bedwars.wins + "             " +  "\n "+ "            Finals: " + player.stats.bedwars.finalKills + "           " + "\n" + ""+ "             FKDR: " + player.stats.bedwars.finalKDRatio + "             ``"}})
+        axios({method: 'patch', url: "https://discordapp.com/api/v6/users/@me", headers: { authorization: config.token, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+  }, data: { bio: "`` 🔴🟢🟡       Stats        ⎯⠀❐ ⤬ `` ``          " + "   " + player.stats.bedwars.level + "⭐                  \n" +                      "             Wins:  "+ player.stats.bedwars.wins + "             " +  "\n "+ "            Finals: " + player.stats.bedwars.finalKills + "           " + "\n" + ""+ "             FKDR: " + player.stats.bedwars.finalKDRatio + "             ``"}})
     }).then(() => {
         console.log("Updated")
     })}, 10000)
